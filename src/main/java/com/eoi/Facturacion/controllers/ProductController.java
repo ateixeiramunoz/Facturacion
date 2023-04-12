09a24b1372e5053ab82e78036cf4ba9f099133e9
Profesor <mail.alejandro.teixeira@gmail.com>
2023-04-12 21:43:30 +0200

    Añadidos
    product-form.html
    product-list.html
    ProductController.java
    
    Corregido Product.java por indicarse por error los nombres de las columnas, lo que causaba un error con el ID. Se añaden tambien getter y setter

---
+++ src/main/java/com/eoi/Facturacion/controllers/ProductController.java
@@ -0,0 +1,46 @@
+package com.eoi.Facturacion.controllers;
+
+import com.eoi.Facturacion.entities.Product;
+import com.eoi.Facturacion.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/products")
+public class ProductController {
+
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping("/")
+    public String showProducts(Model model) {
+        model.addAttribute("products", productService.findAll());
+        return "product-list";
+    }
+
+    @GetMapping("/new")
+    public String showNewProductForm(Model model) {
+        model.addAttribute("product", new Product());
+        return "product-form";
+    }
+
+    @PostMapping("/save")
+    public String saveProduct(@ModelAttribute("product") Product product) {
+        productService.save(product);
+        return "redirect:/products/";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String showEditProductForm(@PathVariable("id") Long id, Model model) {
+        model.addAttribute("product", productService.findById(id));
+        return "product-form";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteProduct(@PathVariable("id") Long id) {
+        productService.deleteById(id);
+        return "redirect:/products/";
+    }
+}
